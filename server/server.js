9b069d3137fa35748e1d4b7b940654892b279cd2
MilkMan90 <mlkaufman747@gmail.com>
2017-01-08 16:11:45 -0700

    serving up static files

---
+++ server/server.js
@@ -0,0 +1,56 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser');
+const cors = require('express-cors');
+var mongoose=require('mongoose');
+var fs = require('fs')
+const path = require('path');
+var dbName = 'studentDB';
+var User = require('./models/user.js')
+const MongoClient = require('mongodb').MongoClient
+
+var connectionString = 'mongodb://localhost:27017/' + dbName;
+
+mongoose.connect(connectionString);
+
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+// app.use('/api')
+
+// Get all users
+app.get('/users', (request, response) => {
+  User.find(function(err, users) {
+  if (err) {
+    response.send(err)
+  }
+  // res.render(view, locals)
+  // res.render('index.ejs', {dinosaurs: dinos})
+  response.send({ users: users });
+  })
+});
+
+app.post('/users', function(req, res) {
+  var user = new User(req.body);
+
+  user.save(function(err) {
+    if (err) {
+      res.send(err)
+    }
+    User.find(function(err, dinos) {
+      // res.render('index.ejs', {dinosaurs: dinos})
+      console.log(dinos);
+    })
+  })
+})
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+app.listen(3000, function () {
+  console.log('RrrarrrrRrrrr server alive on port 3000')
+})
